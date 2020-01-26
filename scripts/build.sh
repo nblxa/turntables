607a204683177a67040ca3a9169d8c2e718c686c
Pavel Mitrofanov <195997+nblxa@users.noreply.github.com>
2020-01-26 22:16:27 +0100

    Deploy to Maven Central

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+RELEASE_PROFILE=""
+SCRPT="[scripts/build.sh]"
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" == master ]]; then
+  echo "$SCRPT Building a new release."
+  RELEASE_PROFILE="-Prelease --settings scripts/release-settings.xml"
+fi
+
+if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+  echo "$SCRPT Building a pull request."
+fi
+
+./mvnw jacoco:prepare-agent \
+       install \
+       jacoco:report \
+       sonar:sonar -Dsonar.projectKey=turntables \
+       coveralls:report \
+       -Derrorprone \
+       -e -B -V $RELEASE_PROFILE
