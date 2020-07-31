8fa7f452e9227bfa7ad04d6d80e4abeede8c3055
Pavel Mitrofanov <pavel@mitrofa.nov.ru>
2020-07-31 10:48:48 +0200

    Extend tests for column/table name character case matching.

--- turntables-core/src/test/java/io/github/nblxa/turntables/test/assertj/TestOrderedRowsAndCols.java
+++ turntables-core/src/test/java/io/github/nblxa/turntables/test/assertj/TestOrderedRowsAndCols.java
@@ -1,13 +1,18 @@
 package io.github.nblxa.turntables.test.assertj;
 
+import io.github.nblxa.turntables.Settings;
+import io.github.nblxa.turntables.SettingsTransaction;
 import io.github.nblxa.turntables.Tab;
 import io.github.nblxa.turntables.Turntables;
+import io.github.nblxa.turntables.Typ;
 import io.github.nblxa.turntables.assertj.TabAssert;
 import io.github.nblxa.turntables.assertj.assertj.AssertAssertJ;
 import org.junit.Test;
 
 import static io.github.nblxa.turntables.Turntables.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+
+import java.math.BigDecimal;
 
 public class TestOrderedRowsAndCols {
   private static final String LS = System.lineSeparator();
@@ -177,5 +182,103 @@ public class TestOrderedRowsAndCols {
             .append("BUT: WAS Table:").append(LS)
             .append("    - col1 : 3").append(LS)
             .append("      col2 : 4").append(LS).toString());
+  }
+
+  @Test
+  public void typeMismatch() {
+    Tab exp = Turntables.tab()
+        .row(1, 2)
+        .row(3, 4);
+    Tab act = Turntables.tab()
+        .row(1, BigDecimal.valueOf(2L))
+        .row(3, BigDecimal.valueOf(4L));
+
+    Throwable t = null;
+    // comment next line to check in the IDE:
+    t = catchThrowable(() -> {
+      tabAssert(exp, act);
+      // comment next line to check in the IDE:
+    });
+    AssertAssertJ.assertThat(t)
+        .isAssertionErrorWithMessage(new StringBuilder(LS)
+            .append("EXPECTED: Table:").append(LS)
+            .append("  cols:").append(LS)
+            .append("      - name : col1").append(LS)
+            .append("        type : integer").append(LS)
+            .append("        key  : false").append(LS)
+            .append("      - name : col2").append(LS)
+            .append("        type : integer").append(LS)
+            .append("        key  : false").append(LS)
+            .append("  rows:").append(LS)
+            .append("      - col1 : 1").append(LS)
+            .append("        col2 : 2").append(LS)
+            .append("      - col1 : 3").append(LS)
+            .append("        col2 : 4").append(LS)
+            .append("BUT: WAS Table:").append(LS)
+            .append("  cols:").append(LS)
+            .append("      - name : col1").append(LS)
+            .append("        type : integer").append(LS)
+            .append("        key  : false").append(LS)
+            .append("      - name : col2").append(LS)
+            .append("        type : decimal").append(LS)
+            .append("        key  : false").append(LS)
+            .append("  rows:").append(LS)
+            .append("      - col1 : 1").append(LS)
+            .append("        col2 : 2").append(LS)
+            .append("      - col1 : 3").append(LS)
+            .append("        col2 : 4").append(LS).toString());
+  }
+
+  @Test
+  public void mismatchCaseInsensitive() {
+    Tab exp = Turntables.tab()
+        .col("A", Typ.INTEGER)
+        .col("b", Typ.INTEGER)
+        .row(1, 2)
+        .row(3, 4);
+    Tab act = Turntables.tab()
+        .col("a", Typ.INTEGER)
+        .col("B", Typ.STRING)
+        .row(1, "2")
+        .row(3, "5");
+
+    Throwable t = null;
+    // comment next line to check in the IDE:
+    t = catchThrowable(() -> {
+      try (SettingsTransaction ignored = Turntables.setSettings(Settings.builder()
+          .nameMode(Settings.NameMode.CASE_INSENSITIVE)
+          .build())) {
+        tabAssert(exp, act);
+      }
+      // comment next line to check in the IDE:
+    });
+    AssertAssertJ.assertThat(t)
+        .isAssertionErrorWithMessage(new StringBuilder(LS)
+            .append("EXPECTED: Table:").append(LS)
+            .append("  cols:").append(LS)
+            .append("      - name : A").append(LS)
+            .append("        type : integer").append(LS)
+            .append("        key  : false").append(LS)
+            .append("      - name : b").append(LS)
+            .append("        type : integer").append(LS)
+            .append("        key  : false").append(LS)
+            .append("  rows:").append(LS)
+            .append("      - A : 1").append(LS)
+            .append("        b : 2").append(LS)
+            .append("      - A : 3").append(LS)
+            .append("        b : 4").append(LS)
+            .append("BUT: WAS Table:").append(LS)
+            .append("  cols:").append(LS)
+            .append("      - name : A").append(LS)
+            .append("        type : integer").append(LS)
+            .append("        key  : false").append(LS)
+            .append("      - name : b").append(LS)
+            .append("        type : string").append(LS)
+            .append("        key  : false").append(LS)
+            .append("  rows:").append(LS)
+            .append("      - A : 1").append(LS)
+            .append("        b : 2").append(LS)
+            .append("      - A : 3").append(LS)
+            .append("        b : 5").append(LS).toString());
   }
 }
