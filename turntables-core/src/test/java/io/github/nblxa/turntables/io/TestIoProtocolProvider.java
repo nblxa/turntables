a84564b9f235f1c92642c8a56c9b580d49409c51
Pavel Mitrofanov <pavel@mitrofa.nov.ru>
2020-11-01 20:02:35 +0100

    Increase the coverage a bit

---
+++ turntables-core/src/test/java/io/github/nblxa/turntables/io/TestIoProtocolProvider.java
@@ -0,0 +1,33 @@
+package io.github.nblxa.turntables.io;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class TestIoProtocolProvider {
+
+  static class TestProvider implements IoProtocolProvider {
+  }
+
+  static IoProtocolProvider prov = new TestProvider();
+
+  @Test
+  public void testIngestionProtocols() {
+    assertThat(prov.ingestionProtocols()).isEmpty();
+  }
+
+  @Test
+  public void testFeedProtocols() {
+    assertThat(prov.feedProtocols()).isEmpty();
+  }
+
+  @Test
+  public void testNameSanitizers() {
+    assertThat(prov.nameSanitizers()).isEmpty();
+  }
+
+  @Test
+  public void testSettingsProtocols() {
+    assertThat(prov.settingsProtocols()).isEmpty();
+  }
+}
